1a5ec7049a8fffd2ea83bd6198d2b94d8313ac67
Caio Theodoro de Carvalho <caio.theodoroc@gmail.com>
2020-04-24 19:21:08 -0300

    Mais js, validações,manipulação de arrays e forms

--- introducao/js/calcula-imc.js
+++ introducao/js/calcula-imc.js
@@ -7,39 +7,51 @@ console.log(pacientes);
 
 
 for(var i =0;i<pacientes.length;i++){
-    var pesoEhValido = true;
-    var alturaEhValida = true;
+
     
     var tdpeso = pacientes[i].querySelector(".info-peso");
     var peso = tdpeso.textContent;  
-
     var tdAltura = pacientes[i].querySelector(".info-altura");
     var altura = tdAltura.textContent;
+    
+
+    var pesoEhValido = validaPeso(peso);
+    var alturaEhValida = validaAltura(altura);
 
     var tdImc = pacientes[i].querySelector(".info-imc");
 
-    if(peso <= 0 || peso >= 1000){
-        console.log("Peso inválido");
-        pesoEhValido = false;
+    if(!pesoEhValido){
+        console.log("Peso inválido");        
         tdImc.textContent = "Peso Inválido";
         pacientes[i].classList.add("paciente-invalido");
        
     }
-    if(altura <= 0 || altura >= 3.00){
-        console.log("Altura inválida");
-        alturaEhValida = false;
+    if(!alturaEhValida){
+        console.log("Altura inválida");        
         tdImc.textContent = "Altura inválida";
-        pacientes[i].classList.add("paciente-invalido");
-        
-        
+        pacientes[i].classList.add("paciente-invalido");                
     }
     if(alturaEhValida && pesoEhValido){
-        var imc =peso /Math.pow(altura,2);
-        tdImc.textContent = imc.toFixed(2);
+       
+        tdImc.textContent = calculaImc(peso,altura);
     }    
 }
+function calculaImc(peso,altura){
+    return (peso/Math.pow(altura,2)).toFixed(2);
+}
+function validaPeso(peso){
+    if(peso >=0 && peso <=1000)
+        return true;
+    else
+        return false;
+}
+function validaAltura(altura){
+    if(altura >0 && altura <= 3.00)
+        return true;
+    else   
+        return false;
+}
 
-titulo.addEventListener("click",function (){console.log("Posso chamar uma função anônima")});
 
 
 
---
+++ introducao/js/form.js
@@ -0,0 +1,78 @@
+var botao = document.querySelector("#adicionar-paciente");
+
+botao.addEventListener("click",function(event){
+    
+    event.preventDefault();
+    
+    var form = document.querySelector("#form-adiciona");    
+    //extrai informações do paciente do form no HTML
+    var paciente = obtemPacienteFormulario(form);
+    
+    //cria a tr e as tds
+    var pacienteTr = montaTr(paciente); 
+    //adiciona paciente na tabela
+    var erro = validaPaciente(paciente);
+    if(erro.length != 0)
+    {
+        console.log(erro)
+        exibeErro(erro);
+        return;
+    }
+    var tabelaPacientes = document.querySelector("#tabela-pacientes");
+    tabelaPacientes.appendChild(pacienteTr);
+
+    form.reset();
+    
+});
+
+function exibeErro(erro) {
+
+    var ul = document.querySelector("#mensagem-de-erro");
+
+    erro.forEach(element => {
+        var li = document.createElement("li");
+        li.textContent = element;
+        ul.appendChild(li);
+    });
+}
+
+function validaPaciente(paciente){
+    var erros = [];
+    if(!validaPeso(paciente.peso)){ 
+        erros.push("Peso é inválido");
+    }    
+    if(!validaAltura(paciente.altura)){
+        erros.push("Altura não é válida")
+    }    
+    return erros;
+}
+
+function obtemPacienteFormulario(form)
+{
+    var paciente = {
+        nome:form.nome.value,
+        peso:form.peso.value,
+        altura:form.altura.value,
+        gordura:form.gordura.value,
+        imc:calculaImc(form.peso.value,form.altura.value)
+    }
+    return paciente;
+}
+function montaTr(paciente){
+    var pacienteTr = document.createElement("tr")
+    pacienteTr.classList.add("paciente");
+
+    pacienteTr.appendChild(montaTd(paciente.nome,"info-nome"));
+    pacienteTr.appendChild( montaTd(paciente.peso,"info-peso"));
+    pacienteTr.appendChild(montaTd(paciente.altura,"info-altura"));
+    pacienteTr.appendChild(montaTd(paciente.gordura,"info-gordura"));
+    pacienteTr.appendChild(montaTd(paciente.imc,"info-imc"));
+
+    return pacienteTr;
+}
+function montaTd(dado,classe){
+    var td = document.createElement("td");
+    td.textContent = dado;
+    td.classList.add(classe);
+    return td;
+}
